24565e1ad158a2cf42c65bc34f299cba28ecc8b6
TGAV <46203536+tpgaviria@users.noreply.github.com>
2019-07-02 18:30:32 -0400

    wip

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({ defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-var routes = require('./controller/routes.js');
+var routes = require('./routes/routes.js/index.js.js');
 app.use(routes);
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/technews"
